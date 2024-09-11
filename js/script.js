6ce6298569ab711c1279d96381c8db838d28dc56
sayzuritrevizan <148063702+sayzuritrevizan@users.noreply.github.com>
2024-09-11 10:19:45 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ function respostaSelecionada(opcaoSelecionada){
 }
 
 function mostraResultado(){
-    caixaPerguntas.textContent = `Em 2049...´;
+    caixaPerguntas.textContent = `Em 2049, $(nome)`;
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
     caixaResultado.classlist.add("mostrar");
