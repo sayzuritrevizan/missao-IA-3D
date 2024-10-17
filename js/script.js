95d92387b0e05539c5200dde44e9fcc65b56355d
sayzuritrevizan <148063702+sayzuritrevizan@users.noreply.github.com>
2024-10-17 08:31:08 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,26 +6,27 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-const  botaoJogarNovamente = document.querySelector(".novamente-btn");
-const botaoInicial = document.querySelector(".iniciar-btn");
+const botaoJogarNovamente = document.querySelector(".novamente-btn"); 
+const botaoIniciar = document.querySelector(".iniciar-btn");
 const telaInicial = document.querySelector(".tela-inicial");
 
 let atual = 0; 
 let perguntaAtual;
 let historiaFinal = "";
 
-botaoIniciar.addEventListener('click',iniciaJogo);
+botaoIniciar.addEventListener('click', iniciaJogo);
 
-function iniciaJogo() { 
+function iniciaJogo() {
     atual = 0;
     historiaFinal = "";
     telaInicial.style.display = 'none';
-    caixaPerguntas.classList.remove(".mostrar");
-    caixaAlternativas.classList.remove(".mostrar");
-    caixaResultado.classList.remove(".mostrar");
+    caixaPerguntas.classList.remove("mostrar");
+    caixaAlternativas.classList.remove("mostrar");
+    caixaResultado.classList.remove("mostrar");
     mostraPergunta();
 }
 
+function mostraPergunta() {
     if(atual >= perguntas.length){
         mostraResultado();
         return;
@@ -48,35 +49,33 @@ function mostraAlternativas(){
 function respostaSelecionada(opcaoSelecionada){
     const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
     historiaFinal += afirmacoes + " ";
-    if(opcaoSelecionada.proxima !== undefined) {
-        atual = opcaoSelecionada.proxima;
-    }else {
-        mostraResultado();
-        return;
-    }
+   if(opcaoSelecionada.proxima !== undefined) {
+       atual = opcaoSelecionada.proxima;
+   }else {
+       mostraResultado();
+       return;
+   }
     mostraPergunta();
 }
 
 function mostraResultado(){
-    caixaPerguntas.textContent = `Em 2049, $(nome)`;
+    caixaPerguntas.textContent = `Após os estudos, ${nome} descobriu que`;
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
-    caixaResultado.classlist.add("mostrar");
-    botaoJogarNovamente.addEventListener("click", jogarNovamente);
+    caixaResultado.classList.add("mostrar"); 
+    botaoJogarNovamente.addEventListener("click", jogarNovamente); 
 }
 
-function jogarNovamente {
+function jogarNovamente(){
     atual = 0;
     historiaFinal = "";
-    caixaResultado.classList.remove("mostrar");
+    caixaResultado.classList.remove("mostrar"); 
     mostraPergunta();
 }
 
-function substituiNome () {
-    for (const pergunta of perguntas) {
+function substituiNome() {
+    for(const pergunta of perguntas) {
         pergunta.enunciado = pergunta.enunciado.replace(/você/g, nome);
     }
 }
-
 substituiNome();
-mostraPergunta();
