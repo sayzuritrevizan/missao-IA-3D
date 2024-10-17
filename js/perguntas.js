a1df4ad812e414c98cbf739c50cf7ae5fb6e7b4c
sayzuritrevizan <148063702+sayzuritrevizan@users.noreply.github.com>
2024-10-17 08:43:17 -0300

    Update perguntas.js

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,63 +1,41 @@
 export const perguntas = [
     {
-        enunciado: "Você pratica esportes",
+        enunciado: "Você lê frequentemente?",
+        alternativas: [
+            {
+                texto: "sim,adoro livros",
+                afirmacao: [
+                    "é muito bom ter este hábito."
+                ],
+                proxima: 1,  // Direciona para a pergunta 2
+            },
+            {
+                texto: "não, acho chato",
+                afirmacao: [
+                    "A leitura é essencial para evolução dos seres humanos ."
+                ],
+                proxima: 1,  // Direciona para a pergunta 2
+            },
+        ]
+    },
+    {
+        enunciado: "Você acha que a escola incentiva a prática de leitura?",
         alternativas: [
             {
                 texto: "sim",
                 afirmacao: [
-                    "qual esporte você gosta",
-                    ""
-                    ],
-                proxoma: 1,
-            },
-            {
-                texto: "não",
-                afirmacao: [
-                    "por qual motivo voce nao pratica",
-                    "afirmacao 2"
-                    ]
-            }          
-           
-        ]
-    },
-    {
-        enunciado: "voce acha importante essa pratica diaria",
-        alternativas: [
-            {
-                texto:"sim",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
+                   "que bom, nosso aprendizado começa na escola.",
+                ],
+                proxima: 2,  // Direciona para a pergunta 3
             },
             {
                 texto: "nao",
                 afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            }
+                    "uma pena...tente colocar isso na seu dia a dia."
+                ],
+                proxima: 2,  // Direciona para a pergunta 3
+            },
         ]
     },
     {
-        enunciado: "Pergunta 3",
-        alternativas: [
-            {
-                texto:"Alternativa 1 da pergunta 3",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            },
-            {
-                texto:"Alternativa 2 da pergunta 3",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            }
-           
-        ]
-    },
-];
-
+        
