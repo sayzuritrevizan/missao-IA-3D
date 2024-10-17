d144f29071e7f251683a35216681aef18aebe461
sayzuritrevizan <148063702+sayzuritrevizan@users.noreply.github.com>
2024-10-17 08:43:55 -0300

    Update aleatorio.js

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,8 +1,8 @@
 const nomes = ["Sayuri", "Pedro", "Amanda", "Ashley", "Luan"];
 
 export function aleatorio (lista){
-  const posicao = Math.floor(Math.random()* lista.lenght);
-  return lisra[posicao];
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
 }
 
 export const nome = aleatorio(nomes)
